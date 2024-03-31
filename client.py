8669ce3018fba85afb1085875e4f6ad0972f4629
Ramanjs <raman.s2662@gmail.com>
2024-03-31 22:45:36 +0530

    complete client

--- client.py
+++ client.py
@@ -13,12 +13,17 @@ while True:
     print("Choose an operation from the following:\n1. SET\n2. GET")
     op = int(input())
     if (op == 1):
-        print("Enter key value pair")
+        print("Enter key value pair:")
         command = input()
         with grpc.insecure_channel(LEADER) as channel:
             stub = raft_pb2_grpc.RaftStub(channel)
             response = stub.ServeClient(raft_pb2.ServeClientRequest(request=f"SET {command}"))
-            print(response)
+            print(response.success)
     elif (op == 2):
-        pass
+        print("Enter key:")
+        key = input()
+        with grpc.insecure_channel(LEADER) as channel:
+            stub = raft_pb2_grpc.RaftStub(channel)
+            response = stub.ServeClient(raft_pb2.ServeClientRequest(request=f"GET {key}"))
+            print(response.data)
     pass
